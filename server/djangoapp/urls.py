5e7d0f7bdbe38effc437d08e10c778dc8878c754
HalaAlshokri <hala@mkka.com>
2023-08-02 16:58:12 -0400

    Added module 2 mods

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,11 +16,13 @@ urlpatterns = [
     path(route='contact_us/', view=views.contact_us, name='contact_us'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
-
+    path('login/', views.login_request, name='login'),
+    
     # path for logout
-
+    path('logout/', views.logout_request, name='logout'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
